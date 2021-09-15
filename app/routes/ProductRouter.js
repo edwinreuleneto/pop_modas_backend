07159a145837df0e7e7a98c2e4052a7c9e4a2e0f
Edwin Reule <edwinreule@gmail.com>
2021-09-15 19:27:11 -0300

    Fix

--- app/routes/ProductRouter.js
+++ app/routes/ProductRouter.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 import { Product } from "../controller";
 
-router.get('/:sku', Product.GetProduct);
 router.get("/listar", Product.GetAllProduct);
 router.get("/categorias", Product.GetCategories);
+router.get('/:sku', Product.GetProduct);
 
 export default router;
