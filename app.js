dee2970ba603d492ffceee1ebf41eee592549880
Edwin Reule <edwinreule@gmail.com>
2021-09-26 01:28:37 -0300

    fix

--- app.js
+++ app.js
@@ -9,28 +9,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 var cors = require('cors');
-var whitelist = [
-  '*'
-];
 
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
-
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
- });
-
-app.use('/api', cors(corsOptionsDelegate), Routes);
+app.use('/api', Routes);
 if(process.env.API_STATUS == 'dev'){
     app.use(express.static(__dirname, { dotfiles: 'allow' } ));
     //RUN DEV
